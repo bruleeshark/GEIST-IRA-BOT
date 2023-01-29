37625ba03863a0187497963d3da10213b4b3dec7
bruleeshark <95983960+bruleeshark@users.noreply.github.com>
2023-01-28 20:14:00 -0700

    v1
    This code defines three structs, InterestRate, Balance, and Trade, which represent the data returned by the blockchain. The structs use the serde crate to support serialization and deserialization, which will make it easy to convert the data returned by the blockchain into these structs for use in your code.
    
    The InterestRate struct has two fields: rate and asset. The rate field is a floating-point number representing the interest rate for the specified asset.
    
    The Balance struct also has two fields: balance and asset. The balance field is a floating-point number representing the balance of the specified asset.
    
    The Trade struct has five fields: buy_amount, sell_amount, buy_asset, sell_asset and timestamp. The buy_amount and sell_amount fields are floating-point numbers representing the amount of assets to buy and sell respectively. The buy_asset and sell_asset fields are strings representing the assets being bought and sold. The timestamp field is an int64 representing the timestamp of the trade.

--- models.rs
+++ models.rs
@@ -1,1 +1,22 @@
-This file would contain the structs or models representing the data returned by the blockchain, such as the interest rates, the balances, etc.
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct InterestRate {
+    pub rate: f64,
+    pub asset: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Balance {
+    pub balance: f64,
+    pub asset: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Trade {
+    pub buy_amount: f64,
+    pub sell_amount: f64,
+    pub buy_asset: String,
+    pub sell_asset: String,
+    pub timestamp: i64,
+}
