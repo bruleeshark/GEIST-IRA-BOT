8ca15733c119a3629f844f27f623a9479d42b429
bruleeshark <95983960+bruleeshark@users.noreply.github.com>
2023-01-28 19:42:14 -0700

    genesis

---
+++ api.rs
@@ -0,0 +1,5 @@
+This file would contain the functions for 
+interacting with the blockchain and web3 providers, 
+such as the functions for querying the interest rates, 
+making the borrow and deposit transactions, 
+etc.
