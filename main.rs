a2cf1201adcea66af2f9482930b631f06326c22b
bruleeshark <95983960+bruleeshark@users.noreply.github.com>
2023-01-28 19:51:42 -0700

    v1
    This main.rs file sets up the configuration using the config crate and reads some parameters, like the threshold for making a trade and the wallet address, from a config.toml file. It also imports the functions from other files that the bot needs to perform its logic.
    
    It starts a loop that will run indefinitely, each iteration it will check for the interest rates, find the best trade using the get_best_trade function and based on the threshold, it will make the borrow and deposit transactions, sign them with the wallet address and finally execute them on the blockchain by calling the execute_transactions function. It then sleeps for a certain period of time before checking again.

--- main.rs
+++ main.rs
@@ -1,1 +1,41 @@
-This file would contain the entry point of your program, where you would set up the necessary dependencies and call the functions that perform the trading logic.
+use std::{env, error::Error};
+use config::{Config, File};
+use api::{get_interest_rate, make_borrow_transaction, make_deposit_transaction};
+use models::{InterestRate, Trade};
+use wallet::{sign_transaction, check_balance};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<String> = env::args().collect();
+    let config = Config::default();
+    config.merge(File::with_name("config")).unwrap();
+
+    let threshold = config.get_float("threshold")?;
+    let wallet_address = config.get_str("wallet_address")?;
+    let web3_provider = config.get_str("web3_provider")?;
+
+    loop {
+        let interest_rates = get_interest_rate(web3_provider)?;
+        let trade = get_best_trade(interest_rates, threshold)?;
+
+        if let Some(t) = trade {
+            let borrow_transaction = make_borrow_transaction(t.borrow_token, t.borrow_amount, web3_provider)?;
+            let deposit_transaction = make_deposit_transaction(t.deposit_token, t.deposit_amount, web3_provider)?;
+            let borrow_signed = sign_transaction(borrow_transaction, wallet_address)?;
+            let deposit_signed = sign_transaction(deposit_transaction, wallet_address)?;
+            execute_transactions(borrow_signed, deposit_signed, web3_provider)?;
+        } else {
+            println!("No profitable trade found.")
+        }
+        // Sleep for a certain period of time before checking again
+        std::thread::sleep(std::time::Duration::from_secs(60));
+    }
+}
+
+fn get_best_trade(interest_rates: Vec<InterestRate>, threshold: f32) -> Result<Option<Trade>, Box<dyn Error>> {
+    // Iterate over the interest rates and check for a profitable trade
+    // Return the trade struct if found, otherwise return None
+}
+
+fn execute_transactions(borrow_signed: String, deposit_signed: String, web3_provider: &str) -> Result<(), Box<dyn Error>> {
+    // Send the signed transactions to the blockchain
+}
