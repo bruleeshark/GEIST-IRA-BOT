8e84e65d8efef3ee038a76cda86ac3a1ee40c4f6
bruleeshark <95983960+bruleeshark@users.noreply.github.com>
2023-01-28 20:21:59 -0700

    v1
    Note that this is a basic example and it depends on the web3 library and the Ethereum blockchain. The sign_transaction function assumes that the private key of the address is already stored in the web3 transport. 
    
    In a real application, the private key should be securely stored and used only for signing transactions.

--- wallet.rs
+++ wallet.rs
@@ -1,1 +1,23 @@
-This file would contain the functions for interacting with the wallet, such as signing transactions, checking the balance, etc.
+use web3::types::{Address, H256, U256};
+use web3::Transport;
+
+pub struct Wallet {
+    pub address: Address,
+    pub transport: Box<dyn Transport>,
+}
+
+impl Wallet {
+    pub fn new(address: Address, transport: Box<dyn Transport>) -> Self {
+        Wallet { address, transport }
+    }
+
+    pub fn balance(&self) -> Result<U256, web3::Error> {
+        let balance = web3::eth::get_balance(&self.transport, self.address, None)?;
+        Ok(balance)
+    }
+
+    pub fn sign_transaction(&self, to: Address, value: U256, gas: U256, gas_price: U256, nonce: U256) -> Result<H256, web3::Error> {
+        let signed_transaction = web3::eth::accounts::sign_transaction(&self.transport, self.address, to, value, gas, gas_price, nonce)?;
+        Ok(signed_transaction.hash)
+    }
+}
