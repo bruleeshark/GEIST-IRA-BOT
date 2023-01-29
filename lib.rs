8c2ee54ae74c3fb9deb406d77972470763b17eb9
bruleeshark <95983960+bruleeshark@users.noreply.github.com>
2023-01-28 19:40:37 -0700

    genesis

---
+++ lib.rs
@@ -0,0 +1,5 @@
+This file would contain the main library of your trading bot, 
+including functions for 
+querying the blockchain for interest rates, 
+checking the spread, 
+and executing the trades.
